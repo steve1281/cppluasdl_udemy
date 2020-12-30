095b0d7e8cbfe096884d17ab004a19dfee6a6ecf
Steve Falcigno <steve1281@hotmail.com>
2020-12-30 16:36:31 -0500

    used GLM for vector2 values

--- 2dgamesengine/src/Game.cpp
+++ 2dgamesengine/src/Game.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "./Constants.h"
 #include "./Game.h"
+#include "../lib/glm/glm.hpp"
 
 Game::Game() {
     this->isRunning = false;
@@ -13,10 +14,8 @@ bool Game::IsRunning() const {
     return this->isRunning;
 }
 
-float projectilePosX = 0.0f;
-float projectilePosY = 0.0f;
-float projectileVelX = 20.0f;
-float projectileVelY = 30.0f;
+glm::vec2 projectilePos = glm::vec2(0.0f, 0.0f);
+glm::vec2 projectileVel = glm::vec2(20.0f, 20.0f);
 
 void Game::Initialize(int width, int height) {
     if (SDL_Init(SDL_INIT_EVERYTHING) !=0) {
@@ -80,8 +79,10 @@ void Game::Update() {
 
     ticksLastFrame = SDL_GetTicks();
 
-    projectilePosX += projectileVelX * deltaTime;
-    projectilePosY += projectileVelY * deltaTime;
+    projectilePos = glm::vec2(
+        projectilePos.x += projectileVel.x * deltaTime,
+        projectilePos.y += projectileVel.y * deltaTime
+    );
 
 }
 
@@ -89,8 +90,8 @@ void Game::Render() {
     SDL_SetRenderDrawColor(renderer, 21,21,21,255);    // set color gray and opaque
     SDL_RenderClear(renderer);   // clear back buffer
     SDL_Rect projectile {
-        (int)projectilePosX,
-        (int)projectilePosY,
+        (int)projectilePos.x,
+        (int)projectilePos.y,
         10,
         10
     };
