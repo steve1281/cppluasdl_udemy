c7360043296396a09daef590d82a202e5f6ecb75
Steve Falcigno <steve1281@hotmail.com>
2021-01-08 13:01:57 -0500

    add keyboard control component

--- 2dgameengine/src/Game.cpp
+++ 2dgameengine/src/Game.cpp
@@ -4,11 +4,13 @@
 #include "./AssetManager.h"
 #include "./Components/TransformComponent.h"
 #include "./Components/SpriteComponent.h"
+#include "./Components/KeyboardControlComponent.h"
 #include "../lib/glm/glm.hpp"
 
 EntityManager manager;
 AssetManager* Game::assetManager = new AssetManager(&manager);
 SDL_Renderer* Game::renderer;
+SDL_Event Game::event;
 
 Game::Game() {
     this->isRunning = false;
@@ -66,6 +68,7 @@ void Game::LoadLevel(int levelNumber) {
     Entity& chopperEntity(manager.AddEntity("chopper"));
     chopperEntity.AddComponent<TransformComponent>(240, 106, 0, 0, 32, 32, 1);
     chopperEntity.AddComponent<SpriteComponent>("chopper-image",2,90,true,false);
+    chopperEntity.AddComponent<KeyboardControlComponent>("up","right","down","left","space");
 
     Entity& radarEntity(manager.AddEntity("radar"));
     radarEntity.AddComponent<TransformComponent>(720, 15, 0, 0, 64, 64, 1);
@@ -76,7 +79,6 @@ void Game::LoadLevel(int levelNumber) {
 }
 
 void Game::ProcessInput() {
-    SDL_Event event;
     SDL_PollEvent(&event);
     switch (event.type) {
         case SDL_QUIT: {
